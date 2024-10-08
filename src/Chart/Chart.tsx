bbb9af000a52fbeccae3ee3fcfb45e1202b7de3e
dumbus <dumbusx@gmail.com>
2024-10-08 17:17:45 +0300

    feat: add chart template using ReactECharts component

--- src/Chart/Chart.tsx
+++ src/Chart/Chart.tsx
@@ -7,30 +7,73 @@ import { ReactECharts } from '../Echarts/ReactECharts';
 
 import ChartService from '../services/ChartService';
 
+import './Chart.css';
+
 // TODO: move generateOption function to utils
 function generateOption(currencyType: ChartType, data: ITransformedData) {
+  let seriesName = '';
   const xAxisData: string[] = [];
   const seriesData: number[] = [];
 
   data[currencyType].forEach((dataItem) => {
-    const { month, value } = dataItem;
+    const { month, value, indicator } = dataItem;
 
     xAxisData.push(month);
     seriesData.push(value);
+    seriesName = indicator;
   });
 
   return {
+    title: {
+      text: 'Заголовок',
+    },
+    grid: {
+      left: '30px',
+      // top: '20px',
+      right: '30px',
+      bottom: '20px',
+      containLabel: true,
+    },
     xAxis: {
       type: 'category',
       data: xAxisData,
+      axisLine: {
+        show: false,
+      },
+      axisTick: {
+        show: false,
+      },
+      boundaryGap: false,
     },
     yAxis: {
       type: 'value',
+      scale: true,
+      splitNumber: 3,
+      splitLine: {
+        lineStyle: {
+          type: 'dashed',
+        },
+      },
+      axisLabel: {
+        showMinLabel: false,
+        margin: 20,
+      },
     },
+    // TODO: change tooltip circle color
+    tooltip: {
+      trigger: 'axis',
+      valueFormatter: (value: number) => value + ' ₽',
+    },
+    animation: false,
     series: [
       {
+        name: seriesName,
         data: seriesData,
         type: 'line',
+        symbol: 'none',
+        lineStyle: {
+          color: '#ff8c00',
+        },
       },
     ],
   };
@@ -38,7 +81,7 @@ function generateOption(currencyType: ChartType, data: ITransformedData) {
 
 function Chart() {
   const [chartData, setChartData] = useState<ITransformedData | []>([]);
-  const [currentChartType, setCurrentChartType] = useState<ChartType>('usd');
+  const [currentChartType, setCurrentChartType] = useState<ChartType>('cny');
   // TODO: remove <any> type
   const [currentOption, setCurrentOption] = useState<any>({});
   const [isLoading, setIsLoading] = useState(true);
@@ -67,8 +110,16 @@ function Chart() {
 
   return (
     <>
-      <Card verticalSpace="xs" horizontalSpace="xs" shadow={false}>
-        <ReactECharts option={currentOption} />
+      <Card
+        className="card"
+        verticalSpace="xs"
+        horizontalSpace="xs"
+        shadow={true}
+      >
+        <ReactECharts
+          option={currentOption}
+          style={{ width: '985px', height: '330px' }}
+        />
       </Card>
     </>
   );
